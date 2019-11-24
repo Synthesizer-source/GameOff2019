15566a01d353784bc35f2ab8e0bfef162fb23a56
hasanozdemir <hasanozdemirfrost01@gmail.com>
2019-11-24 13:27:37 +0300

    ver 1.0

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+
+    public GameObject Player;
+    public float smoothSpeed = 0.3f;
+    private Vector3 currentVelocity;
+
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        Vector3 newPos = new Vector3(transform.position.x, Player.transform.position.y, transform.position.z);
+        
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref currentVelocity, smoothSpeed);
+
+
+    }
+
+}
---
+++ Assets/Scripts/MapManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManagerScript : MonoBehaviour {
+
+    public List<GameObject> platforms;
+
+    public float numberOfPlatforms;
+    public float levelWidth = 8f;
+    public float minY = 3f;
+    public float maxY = 5f;
+    // Use this for initialization
+    void Start () {
+
+        Vector3 spawnPoint = new Vector3();
+
+        for (int i = 0; i < numberOfPlatforms; i++)
+        {
+
+            spawnPoint.y += Random.Range(minY, maxY);
+            spawnPoint.x = Random.Range(-levelWidth, levelWidth);
+            int index = Random.Range(0, 8);
+
+
+            if (index < 8)
+            {
+                Instantiate(platforms[0], spawnPoint, platforms[0].transform.rotation);
+            }
+            else if (index.Equals(8))
+            {
+                Instantiate(platforms[1], spawnPoint, platforms[1].transform.rotation);
+            }
+            else
+            {
+                Instantiate(platforms[2], spawnPoint, platforms[2].transform.rotation);
+            }
+
+        }
+
+    }
+	
+}
